3d5fe378b697b35b0dc1c2bba24c3c452b73657c
Jess Bring-Larsen <jess@bring-larsen.dk>
2022-04-18 21:42:30 +0200

    Experiments

--- processor/src/main/java/org/acme/kafka/processor/QuotesProcessor.java
+++ processor/src/main/java/org/acme/kafka/processor/QuotesProcessor.java
@@ -1,34 +1,49 @@
 package org.acme.kafka.processor;
 
-import java.util.Random;
+import io.quarkus.runtime.Startup;
+import io.quarkus.runtime.StartupEvent;
+import io.smallrye.common.annotation.Identifier;
+import io.smallrye.reactive.messaging.kafka.KafkaClientService;
+import io.smallrye.reactive.messaging.kafka.KafkaConsumer;
+import org.acme.kafka.model.Quote;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 
 import javax.enterprise.context.ApplicationScoped;
-
-import org.acme.kafka.model.Quote;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
-
-import io.smallrye.reactive.messaging.annotations.Blocking;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * A bean consuming data from the "quote-requests" Kafka topic (mapped to "requests" channel) and giving out a random quote.
  * The result is pushed to the "quotes" Kafka topic.
  */
+@Startup
 @ApplicationScoped
 public class QuotesProcessor {
 
     private Random random = new Random();
 
-    @Blocking
-    @Incoming("requests")
-    @Outgoing("quotes")
-    public Quote process(String quoteRequest) throws InterruptedException {
-        Thread.sleep(200);
-        int price = random.nextInt(100);
+    @Inject
+    KafkaClientService clientService;
 
-        if (price < 20) {
-            throw new RuntimeException("Price to low!");
-        }
-        return new Quote(quoteRequest, price);
+    @Inject
+    @Identifier("default-kafka-broker")
+    Map<String, Object> config;
+
+    void onStartup(@Observes StartupEvent startupEvent) {
+        System.out.println("Consumeing..." + config);
+        KafkaConsumer<String, Void> consumer = clientService.getConsumer("requests");
+        consumer.runOnPollingThread(client -> {
+            ConsumerRecords<String, Void> records = client.poll(Duration.ofSeconds(5));
+            while (!records.isEmpty()) {
+                records = client.poll(Duration.ofSeconds(5));
+                records.forEach(r -> {
+                    int price = random.nextInt(100);
+                    System.out.println(new Quote(r.key(), price));
+                });
+            }
+        });
     }
 }
